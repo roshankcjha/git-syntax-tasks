1333516ad4e7768dea76719c8b8561a50cf715ea
Roshan Krishnachandra Jha <roshanjha819@gmail.com>
2020-03-20 20:27:36 +0530

    Maintaining original versions of files

--- file02.js
+++ file02.js
@@ -1,5 +1,7 @@
-function more(){
+// function more(){
 
+// alert("Git is all about branching and merging")
+// alert("knock knock git is coming up")
+// }
+// more()
 alert("Git is all about branching and merging")
-alert("knock knock git is coming up")
-}
